78acdbf3448bae0e73cdeb96d711c056853100a3
yawn west <yawn.west@posteo.eu>
2026-03-28 13:26:48 +0100

    feat: open dir dialog

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
   import { Directory } from "$lib/states/directory.svelte";
   import { Zoom } from "$lib/states/zoom.svelte";
+  import { open } from "@tauri-apps/plugin-dialog";
 
   const zoom = new Zoom();
   const directory = new Directory();
+
+  async function openDir() {
+    const selected = await open({
+      directory: true,
+    });
+    if (selected !== null) {
+      directory.path = selected;
+    }
+  }
 </script>
 
 <svelte:window onkeydown={zoom.handleKeydown} />
@@ -11,6 +21,7 @@
 <main class="container">
   <form class="row">
     <input placeholder="Enter a directory..." bind:value={directory.path} />
+    <button on:click={() => openDir()}>Open ...</button>
   </form>
   <p>{directory.error}</p>
 
