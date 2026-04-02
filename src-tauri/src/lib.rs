f17146ee0c8563acac566000f2a53e986ba3f099
yawn west <yawn.west@posteo.eu>
2026-04-02 16:13:18 +0200

    fix: reload app after update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -141,6 +141,7 @@ pub fn run() {
             app.set_menu(menu)?;
             Ok(())
         })
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
