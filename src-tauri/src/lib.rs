54649d769bb79f0c494fd873ddbce3132b78155d
yawn west <yawn.west@posteo.eu>
2026-04-04 13:38:54 +0200

    feat: keep window position after closing and remove unnecessary dependencies

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -141,11 +141,10 @@ pub fn run() {
             app.set_menu(menu)?;
             Ok(())
         })
-        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![read_dir, remove_dir])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
