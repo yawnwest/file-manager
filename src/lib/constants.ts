d5300cac794adda7f5df4e54d5f15604b1197562
yawn west <yawn.west@posteo.eu>
2026-04-18 09:25:41 +0200

    feat: extended SYSTEM_FILES constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,9 @@
 export const SYSTEM_FILES = new Set([
   ".DS_Store", // macOS folder metadata
   ".localized", // macOS folder localization marker
+  ".Spotlight-V100", // macOS Spotlight index on external volumes
+  ".fseventsd", // macOS FSEvents log on external volumes
+  ".Trashes", // macOS volume trash
   "Thumbs.db", // Windows thumbnail cache
   "desktop.ini", // Windows folder settings
 ]);
