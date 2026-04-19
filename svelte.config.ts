8506d55710b82f971d6945c86c3f80ad7093a36e
yawn west <yawn.west@posteo.eu>
2026-04-19 14:45:31 +0200

    build: changed config files from .js to .ts

--- svelte.config.ts
+++ svelte.config.ts
@@ -2,11 +2,11 @@
 // so we use adapter-static with a fallback to index.html to put the site in SPA mode
 // See: https://svelte.dev/docs/kit/single-page-apps
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
+import type { Config } from "@sveltejs/kit";
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+const config: Config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
